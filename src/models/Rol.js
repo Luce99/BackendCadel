f090fcac794022570f1f793c9a0e00e713a9485b
Lucero <lual-99@hotmail.com>
2022-03-23 00:15:34 -0500

    fixed an error

--- src/models/Rol.js
+++ src/models/Rol.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const permiso = require ("./permiso")
+// const permiso = require ("./permiso")
 
 const  RolSchema = new Schema({
     nombre: {
